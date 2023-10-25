920a1047c56d7d9ebf5356a09c19d212e78b3771
liammcdade <83595569+liammcdade@users.noreply.github.com>
2023-10-25 21:07:56 +0100

    adding my day one and resources

---
+++ python/my work/day 1/math.py
@@ -0,0 +1,35 @@
+#math 
+
+#adding
+print(2 + 3) 
+#subtraction 
+print(3-1)
+#multiplication
+print(2 * 2)
+# division(/)
+print(3 / 2)   
+# exponential(**)
+print(3 ** 2)  
+# modulus(%)
+print(3 % 2)   
+# Floor division operator(//)
+print(3 // 2)  
+
+# data types 
+
+# Int
+print(type(10))                  
+# Float
+print(type(3.14))               
+# Complex
+print(type(1 + 3j))              
+# String
+print(type('Asabeneh'))          
+# List
+print(type([1, 2, 3]))           
+# Dictionary
+print(type({'name':'Asabeneh'})) 
+# Set
+print(type({9.8, 3.14, 2.7}))    
+# Tuple
+print(type((9.8, 3.14, 2.7)))    
